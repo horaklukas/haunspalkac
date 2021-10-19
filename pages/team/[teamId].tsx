0da244befe4201961a9984c288dd5bf06a2ae40d
Lukas Horak <horaklukas@centrum.cz>
2021-10-19 13:14:03 +0200

    🔊 Debug error in loading team data

--- pages/team/[teamId].tsx
+++ pages/team/[teamId].tsx
@@ -15,44 +15,65 @@ import TeamHeader, {
   placeholder as teamHeaderPlaceholder,
 } from "components/TeamHeader";
 import type { UnwrapPromise } from "lib/types";
+import { errorMonitor } from "events";
 
 interface Props extends UnwrapPromise<ReturnType<typeof getTeamData>> {
   fields: FieldsById;
+  error?: Error;
 }
 
 export const getStaticProps: GetStaticProps<Props> = async ({
   params,
   locale,
 }) => {
-  const teamId = getOnlyItem(params.teamId);
-  const [id] = teamId.match(/(\d)*$/) ?? [];
-  const translations = await serverSideTranslations(locale, [
-    "common",
-    "team-detail",
-  ]);
+  try {
+    const teamId = getOnlyItem(params.teamId);
+    const [id] = teamId.match(/(\d)*$/) ?? [];
+    const translations = await serverSideTranslations(locale, [
+      "common",
+      "team-detail",
+    ]);
 
-  const [fields, teamData] = await Promise.all([
-    getFieldsById(),
-    getTeamData(id),
-  ]);
+    const [fields, teamData] = await Promise.all([
+      getFieldsById(),
+      getTeamData(id),
+    ]);
 
-  return {
-    props: {
-      fields,
-      ...teamData,
-      ...translations,
-    },
-    revalidate: 60 * MINUTE,
-  };
+    return {
+      props: {
+        fields,
+        ...teamData,
+        ...translations,
+      },
+      revalidate: 60 * MINUTE,
+    };
+  } catch (error) {
+    return {
+      props: {
+        error,
+      } as Props,
+    };
+  }
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return { paths: [], fallback: true };
 };
 
-const Team = ({ fields, team, schedule, web }: Props) => {
+const Team = ({ fields, team, schedule, web, error }: Props) => {
   const { t } = useTranslation("team-detail");
   const { isFallback: isLoading } = useRouter();
+
+  if (error) {
+    return (
+      <>
+        <h3>{error.message || "NO MESSAGE"}</h3>
+        <pre>{error.stack || "NO STACK"}</pre>
+        <div>{JSON.stringify(error)}</div>
+        {console.log({ error })}
+      </>
+    );
+  }
 
   return (
     <FieldsProvider value={fields}>
