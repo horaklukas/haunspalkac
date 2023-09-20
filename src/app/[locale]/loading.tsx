f366789378e7fbee96c40060debbe3be94dc4d6e
Lukas Horak <horaklukas@centrum.cz>
2023-09-20 20:37:41 +0200

    feat: 💬 Add loading text to the main loader

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,9 +1,14 @@
 import { Loader } from "@/components/ui/Loader";
+import { useTranslations } from "next-intl";
 
 export default function HomeLoading() {
+  const t = useTranslations('app')
+
   return (
     <main className="flex items-center justify-center min-h-screen p-24">
       <Loader />
+      <small className="text-sm">{t('loading')}</small>
+
     </main>
   );
 }
