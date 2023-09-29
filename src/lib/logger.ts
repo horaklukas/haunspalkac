d50d5a8a6a2725eac7eaea5b97fbf81eaa777f67
Lukas Horak <horaklukas@centrum.cz>
2023-09-29 23:11:32 +0200

    refactor: 🔊 Add pretty logging in development mode

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -2,7 +2,10 @@ import { format, transports, createLogger } from "winston";
 
 export const logger = createLogger({
   level: "info",
-  format: format.combine(format.timestamp(), format.json()),
+  format: format.combine(
+    format.timestamp(),
+    process.env.NODE_ENV === "production" ? format.json() : format.prettyPrint()
+  ),
   defaultMeta: { service: "user-service" },
   transports: [new transports.Console()],
 });
